0d408d9da403cd7787a5d21bdf4c92f300e46a4a
Donté Nall <code4zelda@gmail.com>
2020-04-20 14:50:49 -0700

    added auth method

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,22 +1,46 @@
 import React from "react";
 import { gql } from "apollo-boost";
+import { Mutation } from "react-apollo";
 import { useMutation } from "@apollo/react-hooks";
 import { withApollo } from "@apollo/react-hoc";
 import { AUTH_TOKEN } from "../constant";
+import { useHistory } from "react-router";
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
   const [login, setLogin] = React.useState(false);
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [name, setName] = React.useState("");
+  const history = useHistory();
 
-  const _confirm = async () => {
-    // ... you'll implement this 🔜
+  const _confirm = async (data) => {
+    const { token } = login ? data.login : data.signup;
+    _saveUserData(token);
+    history.push(`/`);
   };
 
   const _saveUserData = (token) => {
     localStorage.setItem(AUTH_TOKEN, token);
   };
+
+  const [SIGNUP] = useMutation(SIGNUP_MUTATION);
+  const [LOGIN] = useMutation(LOGIN_MUTATION);
 
   return (
     <div>
@@ -44,9 +68,18 @@ const Login = () => {
         />
       </div>
       <div className="flex mt3">
-        <div className="pointer mr2 button" onClick={() => _confirm()}>
-          {login ? "login" : "create account"}
-        </div>
+        <Mutation
+          mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+          variables={{ email, password, name }}
+          onCompleted={(data) => _confirm(data)}
+        >
+          {(mutation) => (
+            <div className="pointer mr2 button" onClick={mutation}>
+              {login ? "login" : "create account"}
+            </div>
+          )}
+        </Mutation>
+
         <div className="pointer button" onClick={() => setLogin(!login)}>
           {login ? "need to create an account?" : "already have an account?"}
         </div>
